8a35a45a7b9e257d2a301e234c007da0604c54a7
Arpit Jain <arpitkotecha1999@gmail.com>
2019-11-17 18:39:07 +0530

    elimination applied

--- function.py
+++ function.py
@@ -1,6 +1,7 @@
 
 from utils import *
 
+
 def grid_values(grid):
     """Convert grid string into {<box>: <value>} dict with '.' value for empties.
 
@@ -11,9 +12,31 @@ def grid_values(grid):
         - keys: Box labels, e.g. 'A1'
         - values: Value in corresponding box, e.g. '8', or '.' if it is empty.
     """
-    if len(grid) != 81:
-        print("Grid must be 81 length")
-    else:
-        zip_data = zip(boxes, grid)
-        my_grid = dict(zip_data)
-        return my_grid
+    values = []
+    all_digits = '123456789'
+    for value in grid:
+        if value == '.':
+            values.append(all_digits)
+        elif value in all_digits:
+            values.append(value)
+    assert len(values) == 81
+    return dict(zip(boxes, values))
+
+def eliminate(values):
+    """Eliminate values from peers of each box with a single value.
+
+    Go through all the boxes, and whenever there is a box with a single value,
+    eliminate this value from the set of values of all its peers.
+
+    Args:
+        values: Sudoku in dictionary form.
+    Returns:
+        Resulting Sudoku in dictionary form after eliminating values.
+    """
+    for boxKey in values.keys():
+        if len(values[boxKey]) == 1:
+            for peerKey in peers[boxKey]:
+                values[peerKey] = values[peerKey].replace(values[boxKey],'')
+    return values  
+
+display(eliminate(grid_values('..3.2.6..9..3.5..1..18.64....81.29..7.......8..67.82....26.95..8..2.3..9..5.1.3..')))
